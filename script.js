a91480bf7079cfba7cf42d497596efbf6e9c21a4
Semira <nyarkosemiratunatasha@gmail.com>
2024-10-28 17:07:31 +0000

    Developing the Main content

--- script.js
+++ script.js
@@ -21,6 +21,20 @@ window.addEventListener("scroll", () => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
 
+//  add to favorite button toggle
+
+
+const /** {Nodelist} */ $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+
+});
+
+
+
 
 
 
